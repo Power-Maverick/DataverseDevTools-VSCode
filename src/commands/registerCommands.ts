724e2b34382d2883472cacecd411e9b762381c1d
Power-Maverick <danish.naglekar@hotmail.com>
2021-09-05 16:11:52 -0400

    Added AppInsights telemetry to the extension

--- src/commands/registerCommands.ts
+++ src/commands/registerCommands.ts
@@ -1,4 +1,5 @@
 import * as vscode from "vscode";
+import TelemetryReporter from "vscode-extension-telemetry";
 import { CLIHelper } from "../helpers/cliHelper";
 import { UploadHelper } from "../helpers/uploadHelper";
 import { DataverseHelper } from "../helpers/dataverseHelper";
@@ -6,14 +7,17 @@ import { TemplateHelper } from "../helpers/templateHelper";
 import { TypingsHelper } from "../helpers/typingsHelper";
 import { DataverseConnectionTreeItem } from "../trees/dataverseConnectionDataProvider";
 import { EntitiesTreeItem } from "../trees/entitiesDataProvider";
-import { connectionStatusBarUniqueId, extensionPrefix, fileExtensions } from "../utils/Constants";
+import { connectionStatusBarUniqueId, extensionName, extensionPrefix, fileExtensions } from "../utils/Constants";
 import { ICommand, IConnection } from "../utils/Interfaces";
 import { ViewBase } from "../views/ViewBase";
 import { addConnection } from "./connections";
+import { openUri } from "../utils/OpenUri";
 
 let dvStatusBarItem: vscode.StatusBarItem;
+let reporter: TelemetryReporter;
 
-export async function registerCommands(vscontext: vscode.ExtensionContext): Promise<void> {
+export async function registerCommands(vscontext: vscode.ExtensionContext, tr: TelemetryReporter): Promise<void> {
+    reporter = tr;
     const dvHelper = new DataverseHelper(vscontext);
     const views = new ViewBase(vscontext);
     const cliHelper = new CLIHelper(vscontext);
@@ -27,30 +31,64 @@ export async function registerCommands(vscontext: vscode.ExtensionContext): Prom
     const cmds: Array<ICommand> = new Array(
         {
             command: "dvdt.explorer.connections.addConnection",
-            callback: async () => await addConnection(dvHelper),
+            callback: async () => {
+                try {
+                    await addConnection(dvHelper);
+                } catch (error) {
+                    handleErrors(error, "addConnection");
+                }
+            },
         },
         {
             command: "dvdt.explorer.connections.deleteConnection",
-            callback: async (connItem: DataverseConnectionTreeItem) => await dvHelper.deleteConnection(connItem),
+            callback: async (connItem: DataverseConnectionTreeItem) => {
+                try {
+                    await dvHelper.deleteConnection(connItem);
+                } catch (error) {
+                    handleErrors(error, "deleteConnection");
+                }
+            },
         },
         {
             command: "dvdt.explorer.connections.openConnection",
-            callback: (connItem: DataverseConnectionTreeItem) => dvHelper.openEnvironment(connItem),
+            callback: (connItem: DataverseConnectionTreeItem) => {
+                try {
+                    dvHelper.openEnvironment(connItem);
+                } catch (error) {
+                    handleErrors(error, "openConnection");
+                }
+            },
         },
         {
             command: "dvdt.explorer.connections.connectDataverse",
             callback: async (connItem: DataverseConnectionTreeItem) => {
-                const conn = await dvHelper.connectToDataverse(connItem);
-                updateConnectionStatusBar(conn);
+                try {
+                    const conn = await dvHelper.connectToDataverse(connItem);
+                    updateConnectionStatusBar(conn);
+                } catch (error) {
+                    handleErrors(error, "connectDataverse");
+                }
             },
         },
         {
             command: "dvdt.explorer.connections.showConnectionDetails",
-            callback: async (connItem: DataverseConnectionTreeItem) => await dvHelper.showEnvironmentDetails(connItem, views),
+            callback: async (connItem: DataverseConnectionTreeItem) => {
+                try {
+                    await dvHelper.showEnvironmentDetails(connItem, views);
+                } catch (error) {
+                    handleErrors(error, "showConnectionDetails");
+                }
+            },
         },
         {
             command: "dvdt.explorer.entities.showEntityDetails",
-            callback: async (enItem: EntitiesTreeItem) => await dvHelper.showEntityDetails(enItem, views),
+            callback: async (enItem: EntitiesTreeItem) => {
+                try {
+                    await dvHelper.showEntityDetails(enItem, views);
+                } catch (error) {
+                    handleErrors(error, "showEntityDetails");
+                }
+            },
         },
         // {
         //     command: "dvdt.commands.initPlugin",
@@ -58,15 +96,33 @@ export async function registerCommands(vscontext: vscode.ExtensionContext): Prom
         // },
         {
             command: "dvdt.commands.initTS",
-            callback: async (uri: vscode.Uri) => await templateHelper.initiateTypeScriptProject(uri.fsPath),
+            callback: async (uri: vscode.Uri) => {
+                try {
+                    await templateHelper.initiateTypeScriptProject(uri.fsPath);
+                } catch (error) {
+                    handleErrors(error, "initTS");
+                }
+            },
         },
         {
             command: "dvdt.explorer.webresources.uploadWebResource",
-            callback: async (uri: vscode.Uri) => await uploadHelper.uploadWebResource(uri.fsPath),
+            callback: async (uri: vscode.Uri) => {
+                try {
+                    await uploadHelper.uploadWebResource(uri.fsPath);
+                } catch (error) {
+                    handleErrors(error, "uploadWebResource");
+                }
+            },
         },
         {
             command: "dvdt.explorer.entities.generateTyping",
-            callback: async (enItem: EntitiesTreeItem) => await typingHelper.generateTyping(enItem.desc!),
+            callback: async (enItem: EntitiesTreeItem) => {
+                try {
+                    await typingHelper.generateTyping(enItem.desc!);
+                } catch (error) {
+                    handleErrors(error, "generateTyping");
+                }
+            },
         },
     );
     cmds.forEach((c) => {
@@ -85,4 +141,16 @@ export function updateConnectionStatusBar(conn: IConnection | undefined): void {
     } else {
         dvStatusBarItem.hide();
     }
+}
+
+export function handleErrors(err: any, cmd: string) {
+    const btnLogError: vscode.MessageItem = { title: "Log error on GitHub" };
+    vscode.window
+        .showErrorMessage(`${extensionName}: Error occured. Please report it on GitHub with the activity you tried to perform along with Date & Time.`, btnLogError)
+        .then(async (result: vscode.MessageItem | undefined) => {
+            if (result === btnLogError) {
+                await openUri("https://github.com/Power-Maverick/DataverseDevTools-VSCode/issues/new/choose");
+            }
+        });
+    reporter.sendTelemetryException(err, { command: cmd });
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import TelemetryReporter from "vscode-extension-telemetry";
 import { registerCommands } from "./commands/registerCommands";
 import { registerTreeDataProviders } from "./commands/registerTreeDataProviders";
+import { aiKey } from "./utils/Constants";
+
+const extensionId = "danish-naglekar.dataverse-devtools";
+const extension = vscode.extensions.getExtension(extensionId)!;
+const extensionVersion = extension.packageJSON.version;
+
+// telemetry reporter
+let reporter: TelemetryReporter;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // create telemetry reporter on extension activation
+    reporter = new TelemetryReporter(extensionId, extensionVersion, aiKey);
+    // ensure it gets property disposed
+    context.subscriptions.push(reporter);
+
     registerTreeDataProviders(context);
-    registerCommands(context);
+    registerCommands(context, reporter);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    reporter.dispose();
+}
