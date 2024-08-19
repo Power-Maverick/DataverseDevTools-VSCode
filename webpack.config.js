e8ac63de335d0dcf1ca24d4b419d53c33475e9b7
Power-Maverick <danish.naglekar@hotmail.com>
2024-08-19 12:35:39 -0400

    Change webpack config to use node-loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,6 @@ const CopyPlugin = require("copy-webpack-plugin");
 const extensionConfig = {
     target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
     mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-
     entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
@@ -36,7 +35,7 @@ const extensionConfig = {
                 exclude: /node_modules/,
                 use: [
                     {
-                        loader: "ts-loader",
+                        loader: "node-loader",
                     },
                 ],
             },
