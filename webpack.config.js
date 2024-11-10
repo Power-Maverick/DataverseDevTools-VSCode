c5783e1108171d86119bf34db065d204bc0ff29c
Power-Maverick <danish.naglekar@hotmail.com>
2024-11-10 09:05:19 -0500

    WIP - needs testing

--- webpack.config.js
+++ webpack.config.js
@@ -11,27 +11,39 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 /** @type WebpackConfig */
 const extensionConfig = {
-    target: "node", // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
-    mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+    target: "webworker", // vscode extensions run in webworker context for VS Code web 📖 -> https://webpack.js.org/configuration/target/#target
+    mode: "none",
     entry: "./src/extension.ts", // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: {
         // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, "dist"),
         filename: "extension.js",
         libraryTarget: "commonjs2",
+        //devtoolModuleFilenameTemplate: "../[resource-path]",
     },
+    devtool: "nosources-source-map", //nosources-source-map
     externals: {
         vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
         // modules added here also need to be added in the .vscodeignore file
     },
     resolve: {
         // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+        mainFields: ["browser", "module", "main"], // look for `browser` entry point in imported node modules
         extensions: [".ts", ".js"],
+        alias: {
+            // provides alternate implementation for node module and source files
+        },
+        fallback: {
+            // Webpack 5 no longer polyfills Node.js core modules automatically.
+            // see https://webpack.js.org/configuration/resolve/#resolvefallback
+            // for the list of Node.js core module polyfills.
+            path: require.resolve("path-browserify"),
+        },
     },
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.node$/,
                 exclude: /node_modules/,
                 use: [
                     {
@@ -41,7 +53,6 @@ const extensionConfig = {
             },
         ],
     },
-    devtool: "nosources-source-map",
     infrastructureLogging: {
         level: "log", // enables logging required for problem matchers
     },
